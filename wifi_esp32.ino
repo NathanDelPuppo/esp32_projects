b41476588788e1c54cfe57d558a3bf85055a1ebe
Nathan <59972524+NathanDelPuppo@users.noreply.github.com>
2021-01-19 11:12:22 +0100

    Add files via upload
    telegram bot that tell the temperature and humidity thanks to the use of an esp32

---
+++ wifi_esp32.ino
@@ -0,0 +1,112 @@
+#ifdef ESP32
+  #include <WiFi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h> // Universal Telegram Bot Library written by Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+#include <ArduinoJson.h>
+#include "DHT.h"
+
+#define DHTPIN 4     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11   // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// Replace with your network credentials
+const char* ssid = "Livebox-C2B8";
+const char* password = "VkUQZYEtV6HUD6xFES";
+
+// Use @myidbot to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+#define CHAT_ID "1433941197"
+
+// Initialize Telegram BOT
+#define BOTtoken "1573046313:AAHFxwIZeB7FDQ8977iR7tC-xlSNGLW31ww"  // your Bot Token (Get from Botfather)
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+//Checks for new messages every 1 second.
+int botRequestDelay = 1000;
+unsigned long lastTimeBotRan;
+
+       
+// Get BME280 sensor readings and return them as a String variable
+String getReadings(){
+  float temperature, humidity;
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+  String message = " La température est actuellement de: " + String(temperature) + " ºC \n";
+  message += "Humidité: " + String (humidity) + " % \n";
+  return message;
+}
+
+//Handle what happens when you receive new messages
+void handleNewMessages(int numNewMessages) {
+  Serial.println("handleNewMessages");
+  Serial.println(String(numNewMessages));
+
+  for (int i=0; i<numNewMessages; i++) {
+    // Chat id of the requester
+    String chat_id = String(bot.messages[i].chat_id);
+    if (chat_id != CHAT_ID){
+      bot.sendMessage(chat_id, "Unauthorized user", "");
+      continue;
+    }
+    
+    // Print the received message
+    String text = bot.messages[i].text;
+    Serial.println(text);
+
+    String from_name = bot.messages[i].from_name;
+
+    if (text == "/start") {
+      String welcome = "Bienvenue, " + from_name + ".\n";
+      welcome += "Utilise les commandes suivantes afin de lire les valeurs.\n\n";
+      welcome += "/readings \n";
+      bot.sendMessage(chat_id, welcome, "");
+    }
+
+    if (text == "/readings") {
+      String readings = getReadings();
+      bot.sendMessage(chat_id, readings, "");
+    }  
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  #ifdef ESP8266
+    client.setInsecure();
+  #endif
+  
+  // Connect to Wi-Fi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+  digitalWrite(DHTPIN, LOW);
+  pinMode(DHTPIN, OUTPUT);
+  delay(1000);
+  dht.begin();
+}
+
+void loop() {
+  if (millis() > lastTimeBotRan + botRequestDelay)  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while(numNewMessages) {
+      Serial.println("got response");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    lastTimeBotRan = millis();
+  }
+}
